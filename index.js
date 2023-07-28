a2d6488f992e595efcb7686c957e6f0ee1189a5a
didiermun <didiermunezer38@gmail.com>
2023-07-28 14:37:00 +0200

    fix: imports

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import Tokenizer from './Tokenizer/index.mjs'
 import WASM from './Compilers/WASM/index.mjs'
 import SimplerWASM from './Compilers/SimplerWASM/index.mjs'
 import SimpleParser from './SimpleParser/index.mjs'
-import SharedTests from './Tests/Shared/index.mjs'
+import { SIMPLE_IMPORTS } from './Tests/Shared/index.mjs'
 
-export {SimpleParser, Parser, Tokenizer, WASM, SimplerWASM, SharedTests}
+export {SimpleParser, Parser, Tokenizer, WASM, SimplerWASM, SIMPLE_IMPORTS}
 
 //npm publish --access=public
