e0d2fe29d9921d9bfa1cbd9626860e1c36b5496e
didiermun <didiermunezer38@gmail.com>
2023-07-28 13:12:36 +0200

    fix: update version to included native simple imports

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import Tokenizer from './Tokenizer/index.mjs'
 import WASM from './Compilers/WASM/index.mjs'
 import SimplerWASM from './Compilers/SimplerWASM/index.mjs'
 import SimpleParser from './SimpleParser/index.mjs'
+import SharedTests from './Tests/Shared/index.mjs'
 
-export {SimpleParser, Parser, Tokenizer, WASM, SimplerWASM}
+export {SimpleParser, Parser, Tokenizer, WASM, SimplerWASM, SharedTests}
 
 //npm publish --access=public
