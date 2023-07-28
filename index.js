8ea0bd983187982df337c7a8d83090b022c9f2e5
didiermun <didiermunezer38@gmail.com>
2023-07-28 12:24:08 +0200

    fix: simplerwasm

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import Parser from './Parser/index.mjs'
 import Tokenizer from './Tokenizer/index.mjs'
 import WASM from './Compilers/WASM/index.mjs'
+import SimplerWASM from './Compilers/SimplerWASM/index.mjs'
 import SimpleParser from './SimpleParser/index.mjs'
 
-export {SimpleParser, Parser, Tokenizer, WASM}
+export {SimpleParser, Parser, Tokenizer, WASM, SimplerWASM}
 
 //npm publish --access=public
